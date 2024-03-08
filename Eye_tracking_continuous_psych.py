6310957ed46dd9b8794aa0ef6188db362a1f9598
Omer Yildiran <omer.yildiran@ens.psl.eu>
2024-03-08 16:27:05 +0100

    eye tracking with deletion of saccades

--- Eye_tracking_continuous_psych.py
+++ Eye_tracking_continuous_psych.py
@@ -10,17 +10,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import os
-from dva_to_pix import arcmin_to_px, dva_to_px
+from dva_to_pix import arcmin_to_px, dva_to_px,arcmin_to_dva
 from numpy.random import choice as randchoice
 from numpy.random import random, randint, normal, shuffle, choice as randchoice
 from psychopy import sound, gui, visual, core, data, event, logging, clock, colors, layout, iohub, hardware
 
 from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
                                 STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
-from psychopy.tools import monitorunittools
 import sys  # to get file system encoding
 
-from create_conditions import condition_creater
+from create_conditions import condition_creater, create_conditions
 from psychopy import prefs
 from audio_cue import create_stereo_sound, positional_audio
 #from psychopy import microphone
@@ -29,7 +28,9 @@ from psychopy import visual, core, event
 prefs.hardware['audioLib'] = ['pygame']
 from psychopy.hardware import keyboard
 import psychopy.iohub as io
-
+from psychopy.iohub.util import hideWindow, showWindow
+from psychopy.tools.monitorunittools import deg2pix, pix2deg
+from psychopy import monitors
 
 """          Experiment INFO Setup"""
 
@@ -54,18 +55,34 @@ os.chdir(_thisDir)
 # Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
 filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expName, expInfo['date'])
 
-conditions= condition_creater()
+conditions= create_conditions(numOfBlocks=1, blob_widths=[11,17,25], repeats=5)
 # import conditions.npy
 #conditions=np.load('conditions.npy')
 # Create PsychoPy window covering the whole screen
 sizeIs=1024
-win = visual.Window(size=(sizeIs,sizeIs), fullscr=True, monitor='testMonitor', 
+#setup screen properties
+screen_width=37 #31 asuSs 14 # actual size of my screen in cm is 28x17
+screen_height=23 # 16.5 asus
+screen_distance=57
+# define monitor
+labMonitor=monitors.Monitor('labMon', width=37, distance=57)
+labMonitor.setSizePix((sizeIs, sizeIs))
+myMon=monitors.Monitor('asusMon', width=31, distance=57)
+#labMonitor.setgamma(2.4)
+#labMonitor.setwhite([0.95, 0.95, 0.95])  # Example white point with slightly lower values
+win = visual.Window(size=(sizeIs,sizeIs),
+                     fullscr=True, 
+                     monitor=labMonitor, 
                     units='pix', 
                     color=[0, 0, 0],
                       useFBO=True,
-                      screen=1,
+                      screen=0,
                       colorSpace='rgb')
 field_size=[sizeIs,sizeIs]
+### Set the monitor to the correct distance and size
+#win.monitor.setSizePix(field_size)
+win.monitor.setWidth(screen_width)
+win.monitor.setDistance(screen_distance)
 
 frameRate=win.getActualFrameRate()
 print(frameRate)
@@ -76,14 +93,8 @@ else:
     frameDur = 1.0 / 60.0  # could not measure, so guess
 space2pass=keyboard.Keyboard()
 endExpNow = False  # flag for 'escape' or other condition => quit the exp
-#setup screen properties
-screen_width=22 # actual size of my screen in cm is 28x17
-screen_height=17
-screen_distance=57
-### Set the monitor to the correct distance and size
-#win.monitor.setSizePix(field_size)
-win.monitor.setWidth(screen_width)
-win.monitor.setDistance(screen_distance)
+
+
 mouse = event.Mouse(win=win,visible=False)
 # TODO: arcmin to px conversion but open it later
 # def arcmin_to_px(arcmin=1,h=19,d=57,r=1080):
@@ -92,43 +103,73 @@ mouse = event.Mouse(win=win,visible=False)
 frameTolerance = 0.001  # how close to onset before 'same' frame
 
 
-# --- Setup input devices ---
-ioConfig = {}
 
-# Setup eyetracking
-ioConfig['eyetracker.hw.sr_research.eyelink.EyeTracker'] = {
-    'name': 'tracker',
-    'model_name': 'EYELINK 1000 DESKTOP',
-    'simulation_mode': False,
-    'network_settings': '100.1.1.1',
-    'default_native_data_file_name': 'EXPFILE',
-    'runtime_settings': {
-        'sampling_rate': 1000.0,
-        'track_eyes': 'BOTH',
-        'sample_filtering': {
-            'sample_filtering': 'FILTER_LEVEL_2',
-            'elLiveFiltering': 'FILTER_LEVEL_OFF',
-        },
-        'vog_settings': {
-            'pupil_measure_types': 'PUPIL_AREA',
-            'tracking_mode': 'PUPIL_CR_TRACKING',
-            'pupil_center_algorithm': 'ELLIPSE_FIT',
-        }
-    }
-}
+# Eye tracker to use ('mouse', 'eyelink', 'gazepoint', or 'tobii')
+TRACKER = 'eyelink'
+BACKGROUND_COLOR = [0, 0, 0]
+
+devices_config = dict()
+eyetracker_config = dict(name='tracker')
+if TRACKER == 'mouse':
+    eyetracker_config['calibration'] = dict(screen_background_color=BACKGROUND_COLOR,
+                                            auto_pace=True,
+                                            target_attributes=dict(animate=dict(enable=True, expansion_ratio=1.5,
+                                                                                contract_only=False))
+                                            )
+    devices_config['eyetracker.hw.mouse.EyeTracker'] = eyetracker_config
+elif TRACKER == 'eyelink':
+    eyetracker_config['model_name'] = 'EYELINK 1000 DESKTOP'
+    eyetracker_config['simulation_mode'] = False
+    eyetracker_config['runtime_settings'] = dict(sampling_rate=1000, track_eyes='RIGHT')
+    eyetracker_config['calibration'] = dict(screen_background_color=BACKGROUND_COLOR, auto_pace=True)
+    devices_config['eyetracker.hw.sr_research.eyelink.EyeTracker'] = eyetracker_config
+else:
+    print("{} is not a valid TRACKER name; please use 'mouse', 'eyelink', 'gazepoint', or 'tobii'.".format(TRACKER))
+    core.quit()
+
+# # --- Setup input devices ---
+#ioConfig = {}
+
+# # Setup eyetracking
+# ioConfig['eyetracker.hw.sr_research.eyelink.EyeTracker'] = {
+#     'name': 'tracker',
+#     'model_name': 'EYELINK 1000 DESKTOP',
+#     'simulation_mode': False,
+#     'network_settings': '100.1.1.1',
+#     'default_native_data_file_name': 'EXPFILE',
+#     'runtime_settings': {
+#         'sampling_rate': 1000.0,
+#         'track_eyes': 'LEFT',
+#         'sample_filtering': {
+#             'sample_filtering': 'FILTER_LEVEL_2',
+#             'elLiveFiltering': 'FILTER_LEVEL_OFF',
+#         },
+#         'vog_settings': {
+#             'pupil_measure_types': 'PUPIL_AREA',
+#             'tracking_mode': 'PUPIL_CR_TRACKING',
+#             'pupil_center_algorithm': 'ELLIPSE_FIT',
+#         }
+#     }
+# }
 
 # Setup iohub keyboard
-ioConfig['Keyboard'] = dict(use_keymap='psychopy')
+#ioConfig['Keyboard'] = dict(use_keymap='psychopy')
 
 ioSession = '1'
 if 'session' in expInfo:
     ioSession = str(expInfo['session'])
 
-ioServer = io.launchHubServer(window=win, **ioConfig)
-eyetracker = ioServer.getDevice('tracker')
+#ioServer = io.launchHubServer(window=win, **ioConfig)
+ioServer = io.launchHubServer(window=win, **devices_config)
 
+eyetracker = ioServer.getDevice('tracker')
 # create a default keyboard (e.g. to check for escape)
 defaultKeyboard = keyboard.Keyboard(backend='iohub')
+# Display calibration gfx window and run calibration.
+result = eyetracker.runSetupProcedure()
+print("Calibration returned: ", result)
+
+
 
 
 ########################################################################################################################
@@ -138,7 +179,7 @@ defaultKeyboard = keyboard.Keyboard(backend='iohub')
 # Noise properties
 noise_size = field_size  # Use the window size for noise texture
 noise_arcmin = 11  # Standard deviation for pixel noise || noise intensity Adjust to control noise intensity
-noise_std =arcmin_to_px(noise_arcmin,h=screen_height,d=screen_distance,r=field_size[0])# convert arcmin to std for Gaussian
+noise_std =deg2pix(degrees=arcmin_to_dva(noise_arcmin),monitor=win.monitor) #arcmin_to_px(noise_arcmin,h=screen_height,d=screen_distance,r=field_size[0])# convert arcmin to std for Gaussian
 noiseQuantity=120
 class NoiseGenerator:
     def __init__(self, noise_size, noise_intensity=1, noise_quantity=120):
@@ -168,60 +209,6 @@ class NoiseGenerator:
         return noise_objects
 
 
-########################################################################################################################
-### --- Initiate Calibration ---
-# Create some handy timers
-globalClock = core.Clock()  # to track the time since experiment started
-routineTimer = core.Clock()  # to track time remaining of each (possibly non-slip) routine 
-# define target for calibration
-# define parameters for calibration
-calibrationTarget = visual.TargetStim(win, 
-    name='calibrationTarget',
-    radius=0.01, fillColor='', borderColor='black', lineWidth=2.0,
-    innerRadius=0.0035, innerFillColor='green', innerBorderColor='black', innerLineWidth=2.0,
-    colorSpace='rgb', units='height'
-)
-
-# define parameters for calibration
-calibration = hardware.eyetracker.EyetrackerCalibration(win, 
-    eyetracker, calibrationTarget,
-    units=None, colorSpace='rgb',
-    progressMode='time', targetDur=1.5, expandScale=1.5,
-    targetLayout='NINE_POINTS', randomisePos=True, textColor='white',
-    movementAnimation=True, targetDelay=1.0
-)
-# run calibration
-calibration.run()
-# clear any keypresses from during calibration so they don't interfere with the experiment
-defaultKeyboard.clearEvents()
-# the Routine "calibration" was not non-slip safe, so reset the non-slip timer
-routineTimer.reset()
-# define target for validation
-validationTarget = visual.TargetStim(win, 
-    name='validationTarget',
-    radius=5, fillColor='', borderColor='black', lineWidth=2.0,
-    innerRadius=2.5, innerFillColor='green', innerBorderColor='black', innerLineWidth=2.0,
-    colorSpace='rgb', units=None
-)
-# define parameters for validation
-validation = iohub.ValidationProcedure(win,
-    target=validationTarget,
-    gaze_cursor='green', 
-    positions='NINE_POINTS', randomize_positions=True,
-    expand_scale=1.5, target_duration=1.5,
-    enable_position_animation=True, target_delay=1.0,
-    progress_on_key=None, text_color='auto',
-    show_results_screen=True, save_results_screen=False,
-    color_space='rgb', unit_type=None
-)
-# run validation
-validation.run()
-# clear any keypresses from during validation so they don't interfere with the experiment
-defaultKeyboard.clearEvents()
-# the Routine "validation" was not non-slip safe, so reset the non-slip timer
-routineTimer.reset()
-
-### --- Calibration and Validation Finished ---
 
 
 
@@ -271,7 +258,7 @@ def generateBlob(space_constant):
     #blob = (blob - blob.min()) / (blob.max() - blob.min())*2-1 # normalize the blob
     return blob
 # create blobs for each of the conditions
-blob_widths=[11,13,17,21,25,29]
+blob_widths=list(set(conditions))#[11,13,17,21,25,29]
 blobs=[]
 
 
@@ -285,16 +272,15 @@ for i in range(len(blob_widths)):
     blob_dict[blob_widths[i]]=blobs[i]
 
 
-magicNumber=600
 expectedFrameRate=60
-expectedDuration=10 # in seconds
+expectedDuration=20 # in seconds
 expectedFrames=expectedFrameRate*expectedDuration
 
 """                         BLOB MOTION                """ 
 # Blob motion by changing velocity on x and y axis according to the brownian motion
 def generateBrownianMotion(field_size, velocity_std, duration):
     # Generate random velocity for each frame
-    num_frames = int(duration * expectedFrameRate)-1
+    num_frames = int(duration * expectedFrameRate)+1500 # add 600 frames to the duration
     velocies_x=np.random.normal(0, velocity_std, num_frames)
     velocies_y=np.random.normal(0, velocity_std, num_frames)
     
@@ -319,7 +305,6 @@ obs_pointer = visual.Circle(win, radius=10, fillColor='red',colorSpace='rgb', un
 ##    fixation cross for the beginning of the trial (before start of the trial )  
 fixationCross=visual.TextStim(win, text='+', color=[1, 1, 1], units='pix', height=20)
 
-##################### Trials Start Here #####################
 # data to save
 ## blob data
 all_blob_x = []
@@ -329,17 +314,26 @@ all_blob_v=[]
 all_mouse_x = []
 all_mouse_y = []
 all_mouse_v=[]
+### stimulus data
+all_stim_x = []
+all_stim_y = []
+all_stim_v=[]
+jumped=False
 
 
-"""                  Have a REST SCREEN       """
+## --- define have a rest screen ###
 trialNum=1
 haveRest=False
 haveRestText=visual.TextStim(win, text='Press space to continue', color=[1, 1, 1], units='pix', height=20)
 haveRestNum=1
 #####
 sigma_trials=[]
-win.setMouseVisible(False)        
-for blob_width in conditions[:3]:
+win.setMouseVisible(False)    
+
+# create a warning text to indicate the participant to look at the blob if they look 2dva away from the blob
+look_warning=visual.TextStim(win, text='Look at the blob', color=[1, 1, 1], units='pix', height=20)
+
+for blob_width in sorted(conditions):
     sigma_trials.append(blob_width)
     _space2pass_allKeys = []
     space2pass.keys = []
@@ -371,7 +365,6 @@ for blob_width in conditions[:3]:
 
     mouse.y=[]
     mouse.x=[]
-    # create magicNumber frames of stimuli
     # create blob motion positions
     pos_x_obj, pos_y_obj = generateBrownianMotion(field_size=noise_size[0], velocity_std=blob_motion_std, duration=expectedDuration)# pre-generate brownian motion
     pos_x_obj=np.insert(pos_x_obj,0,0)
@@ -389,6 +382,8 @@ for blob_width in conditions[:3]:
     continueRoutine = True
     _timeToFirstFrame = win.getFutureFlipTime(clock="now")
     frameN = -1
+    realFrameN=-1
+
     routineTimer.reset()
     t = 0
     # # wait for 1 second before starting the trial
@@ -399,46 +394,53 @@ for blob_width in conditions[:3]:
         win.flip()
     # draw the stimulus
     blob_obj.setAutoDraw(True)
-    obs_pointer.setAutoDraw(True)
+    obs_pointer.setAutoDraw(False)
     mouse.setVisible(False)  
     response_x=np.empty((expectedFrames))
     response_y=np.empty((expectedFrames))
+    stim_x=np.empty((expectedFrames))
+    stim_y=np.empty((expectedFrames))
     mouse_v=np.empty((expectedFrames))
     checkPointX=0
     checkPointY=0
 
-    # Define the colors two shades of green
-    green_light = np.array([0.5, 1, 0.5])  # Light green
-    green_dark = np.array([0, 0.5, 0])  # Dark green
-    red = np.array([1, 0, 0])  # Red
-    green = np.array([0, 1, 0])  # Green
-    curColor=[1, 1, 1]
-
+    #ioServer.ClearEvents()
     tStart = globalClock.getTime()
     ##################### Trial Start #####################
     eyetracker.setRecordingState(True)  # start recording of gaze
     while continueRoutine:
+        realFrameN=realFrameN+1
         random_noise_index = int(randint(0, noiseQuantity))
-        # t = routineTimer.getTime()
-        #tThisFlip = win.getFutureFlipTime(clock=routineTimer)
-        #tThisFlipGlobal = win.getFutureFlipTime(clock=None)
-        frameN+= 1  # number of completed frames (so 0 is the first frame)
+        #frameN+= 1  # number of completed frames (so 0 is the first frame)
 
-
-        # draw the stimulus
-        #obs_pointer.setPos(mouse.getPos())
-        if eyetracker.getPosition() is not None:
-            obs_pointer.setPos(eyetracker.getPosition())
+        # Get the latest gaze position in display coord space.
+        gpos = eyetracker.getLastGazePosition()
+        valid_gaze_pos = isinstance(gpos, (tuple, list))
+        if valid_gaze_pos:
+            mX=gpos[0]
+            mY=gpos[1]
+            jumped=np.sqrt((mX-obs_pointer.pos[0])**2+(mY-obs_pointer.pos[1])**2)>blob_motion_std*20
+            looksAway=(np.sqrt((pos_x_obj[realFrameN-1]-mX)**2+(pos_y_obj[realFrameN-1]-mY)**2))>blob_motion_std*30
+        if jumped or looksAway or not valid_gaze_pos:
+            pass
         else:
-            obs_pointer.setPos(mouse.getPos())
+            frameN+=1
+        # Update stim based on gaze position
+        # draw the stimulus
+        if valid_gaze_pos and gpos is not None:
+            obs_pointer.setPos(gpos)
         noise_obj[random_noise_index].draw()    # draw noise
-        blob_obj.setPos((pos_x_obj[frameN], pos_y_obj[frameN]))
+        blob_obj.setPos((pos_x_obj[realFrameN], pos_y_obj[realFrameN]))
 
         #save mouse position
-        response_x[frameN]=obs_pointer.pos[0]
-        response_y[frameN]=obs_pointer.pos[1]
+        if not jumped or not looksAway:
+            stim_x[frameN]=blob_obj.pos[0]
+            stim_y[frameN]=blob_obj.pos[1]
+            response_x[frameN]=obs_pointer.pos[0]
+            response_y[frameN]=obs_pointer.pos[1]
         # flip the window
         win.flip()
+
         # end the loop after given seconds
         if frameN > expectedFrames-2:
             continueRoutine = False
@@ -446,21 +448,18 @@ for blob_width in conditions[:3]:
         if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
             endExpNow = True
             continueRoutine = False
-            if eyetracker:
-                eyetracker.setConnectionState(False)
-            #core.quit()
+
+    # the Routine "trial" was not non-slip safe, so reset the non-slip timer
     eyetracker.setRecordingState(False)  # stop recording of gaze
     tEnd = globalClock.getTime()
     print("trial lasteD: "+str(tEnd-tStart))
     blob_obj.setAutoDraw(False)
     obs_pointer.setAutoDraw(False)
     trialNum+=1
-    
-
     t = routineTimer.getTime()
-    
-    print(t)
-    print(frameN)
+    print("time of finish: "+str(t))
+    print("Maximum frame achieved"+str(frameN))
+    print("Maximum frame achieved"+str(realFrameN))
 
     # save positions of target, mouse, and but first calculate velocities
     response_x = np.array(response_x)
@@ -470,19 +469,35 @@ for blob_width in conditions[:3]:
     all_mouse_x.append(response_x)
     all_mouse_y.append(response_y)
     all_mouse_v.append(mouse_v)
+    stim_x = np.array(stim_x)
+    stim_y = np.array(stim_y)
+    stim_v = np.sqrt(np.diff(stim_x)**2 + np.diff(stim_y)**2)
+    stim_v = np.insert(stim_v, 0, 0)
+    # save mouse velocity
+    all_stim_x.append(stim_x)
+    all_stim_y.append(stim_y)
+    all_stim_v.append(stim_v)
     space2pass.keys = None
 
     if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        # Create an Ending Screen
+        endText=visual.TextStim(win, text='Thank you for your participation', color=[1, 1, 1], units='pix', height=20)
+        endText.draw()
+        win.flip()
+        # and wait for participant to press space
+        event.waitKeys()
         win.close()
+        eyetracker.setConnectionState(False)
         break
 
 # Create an Ending Screen
-endText=visual.TextStim(win, text='Thank you for your participation', color=[1, 1, 1], units='pix', height=20)
-endText.draw()
-win.flip()
-# and wait for participant to press space
-event.waitKeys()
-win.close()
+if not endExpNow:
+    endText=visual.TextStim(win, text='Thank you for your participation press any key to exit', color=[1, 1, 1], units='pix', height=20)
+    endText.draw()
+    win.flip()
+    # and wait for participant to press space
+    event.waitKeys()
+    win.close()
 
 ##
 
@@ -491,7 +506,9 @@ win.close()
 #win.close()
 # write mouse positions, velocities, and blob velocities and positions and conditions as a matlab data file
 import scipy.io as sio
+# make conditions as numpy
+conditions=np.array(conditions)
 conditions=conditions.T
 filename = u'data/%s_%s_%s' % (expInfo['participant'], expName, expInfo['date'])
-sio.savemat(filename+'.mat', {'mouse_x': all_mouse_x, 'mouse_y': all_mouse_y, 'response': all_mouse_v, 'blob_x': all_blob_x, 'blob_y': all_blob_y, 'target': all_blob_v, 'sigma': sigma_trials})
+sio.savemat(filename+'.mat', {'mouse_x': all_mouse_x, 'mouse_y': all_mouse_y, 'response': all_mouse_v, 'blob_x': all_stim_x, 'blob_y': all_stim_y, 'target': all_stim_v, 'sigma': sigma_trials})
 core.quit()
